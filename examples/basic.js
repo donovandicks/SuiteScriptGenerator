c6d7b17e5b8cc5ee0a42d40ff7a7fe91836c45f8
Donovan Dicks <donovan.dicks@logmein.com>
2021-05-05 19:51:52 -0400

    Added example files created by CLI

---
+++ examples/basic.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2021 LogMeIn,
+ * 320 Summer St, Boston, MA,
+ * All Rights Reserved.,
+ *,
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
+ * AND CONSTITUTES A VALUABLE TRADE SECRET.,
+ */
+
+/**
+ * @NApiVersion 2.1
+ */
+
+define([
+], () => {
+
+});
---
+++ examples/typed.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2021 LogMeIn,
+ * 320 Summer St, Boston, MA,
+ * All Rights Reserved.,
+ *,
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
+ * AND CONSTITUTES A VALUABLE TRADE SECRET.,
+ */
+
+/**
+ * @NScriptType MapReduceScript
+ * @NApiVersion 2.1
+ */
+
+define([
+], () => {
+
+});
