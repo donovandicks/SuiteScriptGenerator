bc999ff5762127cb95f5b1d03198a90fe93a5445
Donovan Dicks <donovan.dicks@logmein.com>
2021-05-05 15:31:40 -0400

    Initial commit of SuiteScriptGenerator project. A CLI used to generate skeleton files for NetSuite SuiteScripts. Written in Rust.

---
+++ src/main.rs
@@ -0,0 +1,144 @@
+#[macro_use]
+extern crate clap;
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+
+const TYPES: [&'static str; 7] = [
+    "MapReduce",
+    "UserEvent",
+    "Scheduled",
+    "Client",
+    "Suitelet",
+    "Portlet",
+    "RESTlet",
+];
+
+const API: [&'static str; 6] = [
+    "2.1",
+    "2",
+    "2.x",
+    "2.0",
+    "1.0",
+    "1",
+];
+
+const COPYRIGHT: &str = "/**
+ * Copyright (c) 2021 LogMeIn,
+ * 320 Summer St, Boston, MA,
+ * All Rights Reserved.,
+ *,
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
+ * AND CONSTITUTES A VALUABLE TRADE SECRET.,
+ */
+
+";
+
+fn main() {
+    let matches = clap_app!(app =>
+        (version: "0.1.0")
+        (author: crate_authors!())
+        (about: "Generates a SuiteScript file based on the given inputs")
+        (@arg FileName: -f --filename +takes_value +required {validate_file_name} "The name of the SuiteScript file")
+        (@arg ScriptType: -t --stype +takes_value {validate_script_type} "The type of SuiteScript to create")
+        (@arg APIVersion: -v --version +takes_value {validate_api_version} "The API Version to use")
+        (@arg Modules: -m --modules +takes_value +multiple "The N/* modules to import into the project")
+    ).get_matches();
+
+    let file_name = matches.value_of("FileName").unwrap();
+    let mut file = create_file(file_name);
+
+    write_to_file(&mut file, COPYRIGHT);
+    write_to_file(&mut file, "/**\n");
+
+    set_script_type(&mut file, &matches);
+
+    let api = matches.value_of("APIVersion").unwrap_or("2.1");
+    write_to_file(&mut file, format!(" * @NApiVersion {}\n", api).as_ref());
+
+    write_to_file(&mut file, &" */\n\n");
+    write_to_file(&mut file, &"define([\n");
+
+    set_modules(&mut file, &matches);
+
+    write_to_file(&mut file, &"\n});");
+}
+
+fn set_script_type(file: &mut File, matches: &clap::ArgMatches) {
+    let script_type = matches.value_of("ScriptType");
+    match script_type {
+        Some(stype) => {
+            match stype {
+                "MapReduce" | "UserEvent" | "Scheduled" | "Client" => write_to_file(file, format!(" * @NScriptType {}Script\n", stype).as_ref()),
+                _ => write_to_file(file, format!(" * @NScriptType {}\n", stype).as_ref()),
+            }
+        }
+        None => (),
+    }
+}
+
+fn set_modules(file: &mut File, matches: &clap::ArgMatches) {
+    if let Some(modules) = matches.values_of("Modules") {
+        let mods: Vec<&str> = modules.collect();
+        for i in 0..mods.len() {
+            write_to_file(file, format!("  {},\n", mods[i]).as_ref());
+        }
+
+        write_to_file(file, &"], (");
+
+        for i in 0..mods.len() {
+            if i == 0 && i == mods.len() - 1 {
+                write_to_file(file, format!("{}", mods[i]).as_ref());
+            } else if i == 0 {
+                write_to_file(file, format!("{},", mods[i]).as_ref());
+            } else if i == mods.len() - 1 {
+                write_to_file(file, format!(" {}", mods[i]).as_ref());
+            } else {
+                write_to_file(file, format!(" {},", mods[i]).as_ref());
+            }
+        }
+
+        write_to_file(file, &") => {\n");
+    } else {
+        write_to_file(file, &"], () => {\n");
+    }
+}
+
+fn create_file(file_name: &str) -> File {
+    File::create(file_name).unwrap()
+}
+
+fn write_to_file(file: &mut File, contents: &str) {
+    file.write_all(contents.as_bytes()).unwrap();
+}
+
+fn validate_file_name(name: String) -> Result<(), String> {
+    let path = Path::new(&name);
+    
+    if let Some(ext) = path.extension() {
+        if ext != "js" {
+            return Err(String::from("Invalid file type"));
+        }
+    } else {
+        return Err(String::from("File name missing extension"));
+    }
+
+    Ok(())
+}
+
+fn validate_script_type(name: String) -> Result<(), String> {
+    if TYPES.contains(&&name[..]) {
+        return Ok(());
+    }
+
+    Err(String::from("Invalid script type"))
+}
+
+fn validate_api_version(api: String) -> Result<(), String> {
+    if API.contains(&&api[..]) {
+        return Ok(());
+    }
+
+    Err(String::from("Invalid API version"))
+}
+
---
+++ test.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2021 LogMeIn,
+ * 320 Summer St, Boston, MA,
+ * All Rights Reserved.,
+ *,
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
+ * AND CONSTITUTES A VALUABLE TRADE SECRET.,
+ */
+
+/**
+ * @NScriptType RESTlet
+ * @NApiVersion 2.0
+ */
+
+define([
+  log,
+  record,
+  search,
+], (log, record, search) => {
+
+});
