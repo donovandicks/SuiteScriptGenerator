ab12cd2489978dcd3f7253ae280fe6b6e70a748f
Donovan Dicks <donovan.dicks@outlook.com>
2021-05-06 13:21:46 -0400

    Regenerated examples to remove hardcoded copyright

--- examples/imports.js
+++ examples/imports.js
@@ -1,19 +1,10 @@
 /**
- * Copyright (c) 2021 LogMeIn,
- * 320 Summer St, Boston, MA,
- * All Rights Reserved.,
- *,
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
- * AND CONSTITUTES A VALUABLE TRADE SECRET.,
- */
-
-/**
  * @NApiVersion 2.1
  */
 
 define([
-  record,
-  search,
+  'N/record',
+  'N/search',
 ], (record, search) => {
 
 });
--- examples/typed.js
+++ examples/typed.js
@@ -1,13 +1,4 @@
 /**
- * Copyright (c) 2021 LogMeIn,
- * 320 Summer St, Boston, MA,
- * All Rights Reserved.,
- *,
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO LOGMEIN,
- * AND CONSTITUTES A VALUABLE TRADE SECRET.,
- */
-
-/**
  * @NScriptType MapReduceScript
  * @NApiVersion 2.1
  */
